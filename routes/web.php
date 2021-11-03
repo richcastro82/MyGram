aea4c6eccec80c89430ad3d0c63153b0dc0cbd57
richcastro82 <rich.castro@achesongroup.com>
2021-11-03 06:10:25 -0400

    Updates
    Added a profile controller and changed the index path to face the public profile page.

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
 
+
+/*
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+*/
